37b39f0c9ce8d5ce3e1bd956b3127e97d4e601e8
Kqzz <channelktz@gmail.com>
2020-06-20 14:08:40 -0400

    :fire: cleaned up code and made the first run better

--- always_online.py
+++ always_online.py
@@ -1,9 +1,9 @@
 import pyautogui
 import datetime
-import random
 from time import sleep
 from ctypes import Structure, windll, c_uint, sizeof, byref
-
+# ------------------------------------------------------------------
+# This section of code is by FogleBird on stack overflow
 class LASTINPUTINFO(Structure):
     _fields_ = [
         ('cbSize', c_uint),
@@ -17,18 +17,25 @@ def get_idle_duration():
     millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
     return millis / 1000.0
 
+# -------------------------------------------------------------------
+
+first_time = 0
 minutes_until_click = 2
+seconds_until_click = minutes_until_click * 60
 time = str(datetime.datetime.now()).split(" ")[1].split(".")[0]
-print(time)
-x = input("click enter to define pos")
+print(f"the start time is {time}")
+x = input("click enter to define position")
 pos = pyautogui.position()
+print(f"{pos} is the position that will be clicked.")
 while True:
-    print(random.randint(0, 123123123123))
     idle_duration = get_idle_duration()
-    print(idle_duration)
+    print(f"you have been afk for {idle_duration} seconds.")
     if idle_duration >= 420:
         pyautogui.click(pos)
-    sleep(minutes_until_click * 60)
+    if first_time <= 2:
+        seconds_until_click += 1
+    first_time += 1
+    sleep(seconds_until_click)
 
 
 print(get_idle_duration())
